d780056650423dd7912bca095e0611dad5cd0bc3
Mustafa <mustafa_sahinci@hotmail.com>
2022-03-06 07:58:25 +0000

    center the print lines

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 import string
 import os
 words = "jemoeder", "jevader"
+width = os.get_terminal_size().columns
 
 def clearscreen(numlines=100):
     """
@@ -39,37 +40,37 @@ def game():
     lives = 6
 
     while len(word_letters) > 0 and lives > 0:
-        print(f"You have {lives} lives left")
-        print(f"You have used these letters:", " ".join(used_letters))
+        print(f"You have {lives} lives left".center(width))
+        print(f"You have used these letters:".center(width), " ".join(used_letters))
         word_list = [letter if letter in used_letters else "-" for letter in word]
-        print(" ".join(word_list))
+        print(" ".join(word_list).center(width))
 
-        user_letter = input("Guess a letter:\n").upper()
+        user_letter = input("Guess a letter:\n".center(width)).upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
                 clearscreen()
-                print(f"{user_letter} is in the word !!\n")            
+                print(f"{user_letter} is in the word !!\n".center(width))       
             else:
                 lives -= 1
                 clearscreen()
-                print(f"{user_letter} is not in the word\n")          
+                print(f"{user_letter} is not in the word\n".center(width))        
 
         elif user_letter in used_letters:
             clearscreen()
-            print(f"You have already used {user_letter} , Please try again\n")
+            print(f"You have already used {user_letter} , Please try again\n".center(width))
         
         else:
             clearscreen()
-            print("Invalid charachter, Please try again\n")     
+            print("Invalid charachter, Please try again\n".center(width))    
 
     if lives == 0:
         clearscreen()
-        print(f"You died, the word was {word}")
+        print(f"You died, the word was {word}".center(width))
     else:
         clearscreen()
-        print(f"You guessed the word {word} !!")
+        print(f"You guessed the word {word} !!".center(width))
 
 
 
