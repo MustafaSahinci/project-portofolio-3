77524f73afec8a122be467cb810d8d1da1b1d475
Mustafa <mustafa_sahinci@hotmail.com>
2022-03-11 12:02:26 +0000

    remove clearscreen to test on safari

--- run.py
+++ run.py
@@ -76,15 +76,15 @@ def clearscreen(numlines=100):
     Clear the console.
     numlines is an optional argument used only as a fall-back.
     """
-    if os.name == "posix":
-        # Unix/Linux/MacOS/BSD/etc
-        os.system('clear')
-    elif os.name in ("nt", "dos", "ce"):
-        # DOS/Windows
-        os.system('CLS')
-    else:
-        # Fallback for other operating systems.
-        print('\n' * numlines)
+    # if os.name == "posix":
+    #     # Unix/Linux/MacOS/BSD/etc
+    #     os.system('clear')
+    # elif os.name in ("nt", "dos", "ce"):
+    #     # DOS/Windows
+    #     os.system('CLS')
+    # else:
+    #     # Fallback for other operating systems.
+    #     print('\n' * numlines)
 
 
 def random_word(words):
